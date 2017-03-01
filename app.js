01d59d739ff50195ab38d40ae7e173fa796b3413
Matthew Campbell <hyper@hyperworks.nu>
2017-03-01 20:06:57 +0700

    fix server

--- app.js
+++ app.js
@@ -45,9 +45,17 @@ io.on('connection', function(socket) {
 });
 */
 var NRP    = require('node-redis-pubsub');
+/*
 var config = {
-  port  : 6379  , // Port of your locally running Redis server
-  scope : 'demo'  // Use a scope to prevent two NRPs from sharing messages
+  port: 6379
+  host: '139.59.22.165'
+  scope: 'demo'  // Use a scope to prevent two NRPs from sharing messages
+};
+*/
+var config = {
+  port: 6379                        , // Port of your remote Redis server
+  host: '139.59.22.165' , // Redis server host, defaults to 127.0.0.1
+  scope: 'demo'                       // Use a scope to prevent two NRPs from sharing messages
 };
 
 var nrp = new NRP(config); // This is the NRP client
